13376a605371a7b87d68e7f643dfc13ebae58248
Ernie Fabian <erniefabian3rd@gmail.com>
2023-01-19 14:34:48 -0600

    Version with each pottery object fired

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,12 @@
 // Imports go first
 import { makePottery } from './PotteryWheel.js'
+import { firePottery } from './Kiln.js'
 
 
 // Make 5 pieces of pottery at the wheel
 let mug = makePottery("mug", 16, 4)
 //console.log(mug)
 let plate = makePottery("plate", 32, 1)
-//console.log(plate)
 let vase = makePottery("vase", 16, 10)
 let bowl = makePottery("bowl", 22, 6)
 let teapot = makePottery("teapot", 32, 12)
@@ -14,6 +14,13 @@ let teapot = makePottery("teapot", 32, 12)
 
 
 // Fire each piece of pottery in the kiln
+let mugFired = firePottery(mug, 2200)
+//console.log(mugFired)
+let plateFired = firePottery(plate, 2200)
+let vaseFired = firePottery(vase, 2200)
+let bowlFired = firePottery(bowl, 2200)
+let teapotFired = firePottery(teapot, 2200)
+//console.log(teapotFired)
 
 
 // Determine which ones should be sold, and their price
